8df68757129221001824daa323a517ffb018983e
AndrewCook <andrewmcook05@gmail.com>
2026-06-02 11:00:11 +0900

    Login/Logout complete
    The user can now not only log in with Google, but log out as well by pressing the 'User' button after login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import { signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from 'react'
+import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import type {User} from "firebase/auth";
 import { auth, provider} from "./firebase";
 
 import './App.css'
@@ -8,13 +9,34 @@ function login() {
   signInWithPopup(auth, provider);
 }
 
+async function logout() {
+  await signOut(auth);
+}
+
+function profile() {
+
+}
+
 function App() {
+  const [user, setUser] = useState<User | null> (null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
   return (
     <div className="page-container">
       <div className="main-container">
         <div className="top-bar-container">
-          <button onClick={login}>Log in with Google</button>
+          {user ? (
+            <button className="profile" onClick={logout}>User</button>
+          ) : (
+            <button onClick={login}>Login</button>
+          )}
+
+          
         </div>
         <div className="title-container">
           <h1 className="title-text">
